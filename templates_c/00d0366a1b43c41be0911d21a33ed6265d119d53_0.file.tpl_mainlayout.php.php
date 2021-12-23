88b7be2323ead758323c5f1697a4a81e77fc2fa8
fuadzaidan7 <fuadzaidan7@gmail.com>
2021-12-23 08:58:34 +0800

    title-change

--- templates_c/00d0366a1b43c41be0911d21a33ed6265d119d53_0.file.tpl_mainlayout.php.php
+++ templates_c/00d0366a1b43c41be0911d21a33ed6265d119d53_0.file.tpl_mainlayout.php.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.0.0, created on 2021-12-23 00:25:36
+/* Smarty version 4.0.0, created on 2021-12-23 01:58:16
   from 'C:\xampp\htdocs\mettic\templates\tpl_mainlayout.php' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.0.0',
-  'unifunc' => 'content_61c3b3f080df50_12683382',
+  'unifunc' => 'content_61c3c9a858b405_80237570',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '00d0366a1b43c41be0911d21a33ed6265d119d53' => 
     array (
       0 => 'C:\\xampp\\htdocs\\mettic\\templates\\tpl_mainlayout.php',
-      1 => 1640215528,
+      1 => 1640221095,
       2 => 'file',
     ),
   ),
@@ -20,13 +20,13 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_61c3b3f080df50_12683382 (Smarty_Internal_Template $_smarty_tpl) {
+function content_61c3c9a858b405_80237570 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE HTML>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>Architect Template</title>
+		<title>Mettic Systeme</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
